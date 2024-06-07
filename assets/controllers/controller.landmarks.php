2be9cd5185abfe202201bbff33a7be74e1c9f2dc
SRNM21 <gregorio1nonly@gmail.com>
2024-06-07 22:17:15 +0800

    fix

--- assets/controllers/controller.landmarks.php
+++ assets/controllers/controller.landmarks.php
@@ -1,3 +1,39 @@
 <?php
 
+$images = [
+    'cityhall.jpg',
+    'tanghalan.jpg',
+    'palengke.png',
+    'pasig-relv.png',
+    'sannics.png',
+    'alex-lugawan.png',
+];
+
+$name = [
+    'Pasig City Hall',
+    'Tanghalang Pasigueño',
+    'Pasig Mega Market',
+    'Pasig Revolving Tower',
+    'San Nicolas Barangay Hall',
+    'Alex Lugawan & Sizzling'
+];
+
+$desc = [
+    "The City Hall is a stunning vision coming onto Caruncho Avenue, with its ivory-and-emerald façade. The ground floor is almost entirely devoted to a parking area, with the exception of the Cooperative Development Office. The second floor is occupied by the Business Permit and Licensing Office and the Treasury Department.",
+    "Tanghalang Pasigueño is more than just a majestic venue for events. It is a symbol of Pasig City's drive for excellence. It is a tribute to the city's glorious past. And it celebrated the Pasigueños passion for culture and the arts.",
+    "Pasig Mega Market, also known as Pasig City Public Market, is a bustling market located in the city of Pasig, within the metropolitan area of Manila, Philippines. It is one of the largest and busiest public markets in the country.",
+    "Formerly the Mutya ng Pasig Tower, Pasig Revolving Tower was built as an 11-storey building in 1974 in an area then covered with rice paddies and some houses. The structure was built near the Mutya ng Pasig public market during the administration of then-Pasig Mayor Emiliano Caruncho Jr..",
+    "San Nicolas Barangay Hall is a town hall in Pasig, Eastern Manila District, Metro Manila located on M. H. del Pilar Street. San Nicolas Barangay Hall is situated close to the government office San Nicolas Barangay Council and the community center Santa Rosa Multi-Purpose Hall.",
+    " Alex Lugawan offers a variety of other Filipino comfort foods. The warm, welcoming atmosphere and consistent quality make it a popular choice for those looking for a hearty meal at a reasonable price.",
+];
+
+$map = [
+    'https://maps.app.goo.gl/vU8vccHJXg6EpEcA8',
+    'https://maps.app.goo.gl/Q3PPdXqauPXP5Vvt6',
+    'https://maps.app.goo.gl/naxgpDbBsC9h9yb59',
+    'https://maps.app.goo.gl/1J87ZGxCfHnjVh2F8',
+    'https://maps.app.goo.gl/3bEyEy4oGnGSGAaz8',
+    'https://maps.app.goo.gl/oFF7SytpeAEnZkgCA'
+];
+
 require getPublicView('landmarks');
--- views/public/public.landmarks.php
+++ views/public/public.landmarks.php
@@ -5,20 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landmarks | Barangay San Nicolas</title>
     <link rel='icon' type='image/x-icon' href=<?= getFavicon('favicon'); ?>>
-    <link rel='stylesheet' href=<?= getStyle('events'); ?>>
+    <link rel='stylesheet' href=<?= getStyle('landmarks'); ?>>
 </head>
 <body>
     <?php require getPartial('header') ?>
-    <section class='f-center f-col event-section'>
-        <div class='f-col event-content-wrapper'>
-            <div class='f-center f-col event-sec-header'>
+    <section class='f-center f-col landmarks-section'>
+        <div class='f-col landmarks-content-wrapper'>
+            <div class='f-center f-col landmarks-sec-header'>
                 <h2>BARANGAY SAN NICOLAS' LANDMARKS</h2>
             </div>
-            <div class='f-center f-col events-wrapper'>
+            <div class='f-center f-row landmarks-wrapper'>
                 
+                <?php for ($i = 0; $i < count($name); $i++) { ?>
+
+                <div class='landmark-card'>
+                    <div class='landmark-img'>
+                        <img src="<?= getImage($images[$i]) ?>" alt="">
+                    </div>
+                    <div class='f-center f-row landmark-details'>
+                        <h2><?= $name[$i] ?></h2>
+                        <a href=<?= $map[$i] ?> target='_blank' class='f-center'><?= getSVG('map') ?> Maps</a>
+                    </div>
+                    <div class='landmark-description'>
+                        <p><?= $desc[$i] ?></p>
+                    </div>
+                </div>
+
+                <?php } ?>
+
             </div>
         </div>
-        <footer class='f-row events-footer'>
+        <footer class='f-row landmarks-footer'>
             <p>&copy 2024 Pamantasan ng Lungsod ng Pasig, BSIT 2A - Group 3. All rights reserved.</p>
         </footer>
     </section>
